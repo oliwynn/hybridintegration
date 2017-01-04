2e2ace7037539e844872eeff5bf912246e1d6c75
edshee <edshee1@yahoo.co.uk>
2017-01-04 16:39:22 +0000

    started scaffolding the site front page

---
+++ public/main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    products: [{
+      name: 1
+    },{
+      name: 2
+    },{
+      name: 3
+    },{
+      name: 4
+    },{
+      name: 5
+    },{
+      name: 6
+    },{
+      name: 7
+    },{
+      name: 8
+    },{
+      name: 9
+    }]
+  }
+})
