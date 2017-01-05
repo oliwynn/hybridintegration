c26790f77947682597bfc2bc3967d49d366db24f
edshee <edshee1@yahoo.co.uk>
2017-01-05 11:03:39 +0000

    added weather api endpoint

--- app.js
+++ app.js
@@ -12,17 +12,77 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+//request module
+var request = require('request');
+
 // create a new express server
 var app = express();
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+var weather_host = appEnv.services["weatherinsights"] ?
+    appEnv.services["weatherinsights"][0].credentials.url // Weather credentials passed in
+    :
+    ""; // or copy your credentials url here for standalone
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+app.get('/api', function(req, res) {
+    res.send('Hello from the threads API');
+});
+
+function weatherAPI(path, qs, done) {
+    var url = weather_host + path;
+    console.log(url, qs);
+    request({
+        url: url,
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8",
+            "Accept": "application/json"
+        },
+        qs: qs
+    }, function(err, req, data) {
+        if (err) {
+            done(err);
+        } else {
+            if (req.statusCode >= 200 && req.statusCode < 400) {
+                try {
+                    done(null, JSON.parse(data));
+                } catch (e) {
+                    console.log(e);
+                    done(e);
+                }
+            } else {
+                console.log(err);
+                done({
+                    message: req.statusCode,
+                    data: data
+                });
+            }
+        }
+    });
+}
+
+app.get('/api/forecast/daily', function(req, res) {
+    var geocode = (req.query.geocode || "45.43,-75.68").split(",");
+    weatherAPI("/api/weather/v1/geocode/" + geocode[0] + "/" + geocode[1] + "/forecast/daily/10day.json", {
+        units: req.query.units || "m",
+        language: req.query.language || "en"
+    }, function(err, result) {
+        if (err) {
+            console.log(err);
+            res.send(err).status(400);
+        } else {
+            console.log("10 days Forecast");
+            res.json(result);
+        }
+    });
+});
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
 });
--- public/main.js
+++ public/main.js
@@ -1,24 +1,66 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    products: [{
-      name: 1
-    },{
-      name: 2
-    },{
-      name: 3
-    },{
-      name: 4
-    },{
-      name: 5
-    },{
-      name: 6
-    },{
-      name: 7
-    },{
-      name: 8
-    },{
-      name: 9
-    }]
-  }
+    el: '#app',
+    data: {
+        products: [{
+            name: 1
+        }, {
+            name: 2
+        }, {
+            name: 3
+        }, {
+            name: 4
+        }, {
+            name: 5
+        }, {
+            name: 6
+        }, {
+            name: 7
+        }, {
+            name: 8
+        }, {
+            name: 9
+        }]
+    },
+    methods: {
+        weatherAPI: function(path, qs, done) {
+            $.ajax({
+                url: path,
+                type: 'GET',
+                contentType: 'application/json',
+                data: qs,
+                success: function(data) {
+                    if (data.message == 401) {
+                        try {
+                            data.data = JSON.parse(data.data);
+                        } catch (e) {}
+                        done(data);
+                    } else {
+                        done(null, data);
+                    }
+                },
+                error: function(xhr, textStatus, thrownError) {
+                    done(textStatus);
+                }
+            });
+        }
+    }
 })
+
+/*
+weatherAPI("/api/forecast/daily", {
+    geocode: geocode,
+    units: units,
+    language: language
+}, function(err, data) {
+    if (err) {
+        showError('#daily_throbber', '#daily_error', '#daily_display', err);
+    } else {
+        if (data.forecasts) {
+            showDisplay('#daily_throbber', '#daily_error', '#daily_display');
+            displayDaily(data);
+        } else {
+            showError('#daily_throbber', '#daily_error', '#daily_display', "Data missing");
+        }
+    }
+});
+*/
